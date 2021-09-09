37ccbd80cb791aa20fecd5d4494eabdc97e8906e
per2 <per2>
2021-09-09 10:10:12 -0700

    just logs and wireing

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.stopwatch
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+    lateinit var start: Button
+    lateinit var reset: Button
+    lateinit var stopwatch: Chronometer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        wireWidgets()
+
     }
+
+    private fun wireWidgets() {
+        start = findViewById(R.id.button_main_start)
+        reset = findViewById(R.id.button_main_reset)
+        stopwatch = findViewById(R.id.chronometer_main_stopwatch)
+    }
+
+    companion object {
+        val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+
+
 }
